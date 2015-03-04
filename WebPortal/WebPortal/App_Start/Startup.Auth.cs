f5aedbc56e1cd81dc8d3bed0487e84125abd4cf4
deerhit45 <deerhit45@comcast.net>
2015-03-04 15:57:58 -0500

    this is almost working. it gets the newsfeed object now i need to display it

--- WebPortal/WebPortal/App_Start/Startup.Auth.cs
+++ WebPortal/WebPortal/App_Start/Startup.Auth.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿// AppId = "1549521788629862",
+//AppSecret = "3072d557ae33bd64013e58ed3dc44006",
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.DataProtection;
 using Owin;
+using System;
 using WebPortal.Models;
+using Microsoft.Owin.Security.Facebook;
+using System.Collections.Generic;
 
 namespace WebPortal
 {
@@ -14,36 +19,24 @@ namespace WebPortal
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
+            // Configure the UserManager
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
+                        validateInterval: TimeSpan.FromMinutes(20),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
@@ -53,16 +46,30 @@ namespace WebPortal
             //app.UseTwitterAuthentication(
             //   consumerKey: "",
             //   consumerSecret: "");
+            
+            var x = new FacebookAuthenticationOptions();
+            x.Scope.Add("email");
+            x.Scope.Add("friends_about_me");
+            x.Scope.Add("friends_photos");
+            x.AppId = "1549521788629862";
+            x.AppSecret = "3072d557ae33bd64013e58ed3dc44006";
+            x.Provider = new FacebookAuthenticationProvider()
+           {
+               OnAuthenticated = async context =>
+               {
+                   //Get the access token from FB and store it in the database and
+                   //use FacebookC# SDK to get more information about the user
+                   context.Identity.AddClaim(
+                   new System.Security.Claims.Claim("FacebookAccessToken",
+                                                        context.AccessToken));
+               }
+           };
+            x.SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie;
+            
+            
+            app.UseFacebookAuthentication(x);
 
-            app.UseFacebookAuthentication(
-               appId: "1549521788629862",
-               appSecret: "3072d557ae33bd64013e58ed3dc44006");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            //app.UseGoogleAuthentication();
         }
     }
 }
