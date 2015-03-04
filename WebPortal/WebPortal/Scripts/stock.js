312622ca53bccc6c5f71b57fe4b10d17c5947c31
David Wilson <dmw9070@rit.edu>
2015-03-04 17:55:45 -0500

    added angular and stock app/ctrl

---
+++ WebPortal/WebPortal/Scripts/stock.js
@@ -0,0 +1,21 @@
+﻿angular
+    .module('stock', ['ngResource'])
+    .factory('TopFive', ['$resource',
+        function ($resource) {
+            return $resource('http://dev.markitondemand.com/Api/v2/Quote/json');
+        }])
+    .controller('StockCtrl', ['$scope', '$q', 'TopFive',
+        function ($scope, $q, TopFive) {
+            console.log("here");
+            //TODO refresh periodically
+            $q.all([
+                TopFive.query({ symbol: 'AAPL' }).$promise,
+                TopFive.query({ symbol: 'MSFT' }).$promise,
+                TopFive.query({ symbol: 'GOOG' }).$promise,
+                TopFive.query({ symbol: 'AMZN' }).$promise,
+                TopFive.query({ symbol: 'FB' }).$promise
+            ]).then(function (results) {
+                console.log(results);
+                $scope.stocks = results;
+            });
+        }]);
