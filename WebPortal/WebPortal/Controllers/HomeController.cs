f753f2eb81c607d019d521d57964e2a92541c5f5
Edward Dinki <ejdinki@gmail.com>
2015-03-07 16:37:06 -0500

    HomeController Index loads FacebookFeed partial. HomeController also loads the facebook stuff now instead of AccountController doing it
    Needs a lot of clean up work and handling different types of posts
    (status update/sharing stuff/ tagging people/etc are not all in the same
    simple Name and Message format

--- WebPortal/WebPortal/Controllers/HomeController.cs
+++ WebPortal/WebPortal/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
+using System.Threading.Tasks;
 
 namespace WebPortal.Controllers
 {
@@ -20,12 +21,58 @@ namespace WebPortal.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private AccountController ac = new AccountController();
-        private System.Threading.Tasks.Task<ActionResult> test;
-        [Authorize]
-        public ActionResult Index()
+        // Commented out stuff is from Greg. Leaving it here in case it's needed again
+        // Adding UserManager directly in this controller I *think* elimnated the need of AccountController -EJ
+        //private AccountController ac = new AccountController();
+        //private System.Threading.Tasks.Task<ActionResult> test;
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
         {
-            return View(this.ac.FacebookFeed());
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [Authorize]
+        //public ActionResult Index()
+        public async Task<ActionResult> Index()
+        {
+            //return View(this.ac.FacebookFeed()); // Original Code
+            //var claimsforUser = await ac.UserManager.GetClaimsAsync(User.Identity.GetUserId()); // Code copied from User Manager
+            var claimsforUser = await UserManager.GetClaimsAsync(User.Identity.GetUserId()); // Copied UserManager stuff from account controller so we wouldn't need to use it
+            var access_token = claimsforUser.FirstOrDefault(x => x.Type == "FacebookAccessToken").Value;
+            var fb = new FacebookClient(access_token);
+            fb.AppId = 1549521788629862 + "";
+            fb.AppSecret = "3072d557ae33bd64013e58ed3dc44006";
+            dynamic myInfo = fb.Get("/me/home");
+            var friendsList = new List<FacebookFeedModel>();
+            foreach (dynamic post in myInfo.data)
+            {
+                if (post.likes != null)
+                {
+                    friendsList.Add(new FacebookFeedModel()
+                    {
+                        Name = post.from.name,
+                        Message = post.message,
+                        Likes = post.likes.data.Count
+                    });
+                }//if
+                else {
+                    friendsList.Add(new FacebookFeedModel
+                    {
+                        Name = post.from.name,
+                        Message = post.message,
+                        Likes = 0
+                    });
+                }
+            }
+
+            return View(friendsList);
         }
 
         public ActionResult About()
