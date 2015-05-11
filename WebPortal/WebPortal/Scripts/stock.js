6f263dee5fc1780c212831d8d62ee52c1696e29f
David Wilson <dmw9070@rit.edu>
2015-05-10 21:18:47 -0400

    advanced filtering for transactions

--- WebPortal/WebPortal/Scripts/stock.js
+++ WebPortal/WebPortal/Scripts/stock.js
@@ -121,11 +121,46 @@ $.fn.dataTable.ext.search.push(
             var profit = parseFloat(data[5].replace(/[()$]/g, '')) || 0;
             if ((!isNaN(minProfit) && minProfit > profit) || (!isNaN(maxProfit) && profit > maxProfit))
                 return false;
-
-            return true;
         } else {
-            return true; //TODO
+            // date
+            var dateStart = new Date($('#filter-transactions-date-start').val());
+            var dateEnd = new Date($('#filter-transactions-date-end').val());
+            var date = new Date(data[0]);
+            if ((dateStart != null && dateStart != "" && dateStart > date) || (dateEnd != null && dateEnd != "" && dateEnd < date))
+                return false;
+
+            // symbol
+            var symbol = $('#filter-transactions-symbol').val();
+            if (symbol != null && symbol !== "" && data[1].toUpperCase().indexOf(symbol.toUpperCase()) == -1)
+                return false;
+
+            // type
+            var type = $('#filter-transactions-type').val();
+            if (type != null && type !== "" && data[2] !== type)
+                return false;
+
+            // amount
+            var minAmt = parseInt($('#filter-transactions-amt-min').val(), 10);
+            var maxAmt = parseInt($('#filter-transactions-amt-max').val(), 10);
+            var amt = parseFloat(data[3]) || 0;
+            if ((!isNaN(minAmt) && minAmt > amt) || (!isNaN(maxAmt) && amt > maxAmt))
+                return false;
+
+            // price
+            var minPrice = parseFloat($('#filter-transactions-price-min').val());
+            var maxPrice = parseFloat($('#filter-transactions-price-max').val());
+            var price = parseFloat(data[4].replace(/[$,]/g, '')) || 0;
+            if ((!isNaN(minPrice) && minPrice > price) || (!isNaN(maxPrice) && price > maxPrice))
+                return false;
+
+            // total
+            var minTotal = parseFloat($('#filter-transactions-total-min').val());
+            var maxTotal = parseFloat($('#filter-transactions-total-max').val());
+            var total = parseFloat(data[5].replace(/[$,]/g, '')) || 0;
+            if ((!isNaN(minTotal) && minTotal > total) || (!isNaN(maxTotal) && total > maxTotal))
+                return false;
         }
+        return true;
     }
 );
 
@@ -135,14 +170,18 @@ function dataTableStocks() {
         '#filter-stocks-amt-min, #filter-stocks-amt-max, ' +
         '#filter-stocks-price-min, #filter-stocks-price-max, ' +
         '#filter-stocks-change-min, #filter-stocks-change-max, ' +
-        '#filter-stocks-profit-min, #filter-stocks-profit-max').keyup(function () {
+        '#filter-stocks-profit-min, #filter-stocks-profit-max').on( 'keyup change', function () {
         table.draw();
     });
 }
 
 function dataTableTransactions() {
     var table = $('#transactions').DataTable();
-    $('#filter-transactions-symbol').keyup(function () {
+    $('#filter-transactions-date-start, #filter-transactions-date-end, ' +
+        '#filter-transactions-symbol, #filter-transactions-type, ' +
+        '#filter-transactions-amt-min, #filter-transaction-amt-max, ' +
+        '#filter-transactions-price-min, #filter-transactions-price-max, ' +
+        '#filter-transactions-total-min, #filter-transactions-total-max').on( 'keyup change', function () {
         table.draw();
     });
 }
